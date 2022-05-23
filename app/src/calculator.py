aa9478af3a7cfe389e0246a6f5dbdf6aa0aeb1fd
Cuicui Song <cuicuisong@MacBook-Pro-5.local>
2022-05-23 13:42:09 -0500

    correct mistake

--- app/src/calculator.py
+++ app/src/calculator.py
@@ -3,8 +3,8 @@ class Calculator():
     
     def add(self, num1, num2):
 	
-       # return num1+num2
-        return 2
+        return num1+num2
+       # return 2
     def subtract(self, num1, num2):
         return num1-num2
     def multiply(self, num1, num2):
